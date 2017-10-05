a55abca081d45c1311c123a3002b1828cd613779
kdot23 <30609252+kdot23@users.noreply.github.com>
2017-10-05 00:42:06 -0500

    #proud. uses fileInputStream and fileOutputStream

--- FilePartitioner.java
+++ FilePartitioner.java
@@ -1,21 +1,22 @@
 import java.io.File;
-import java.nio.file.Paths;
-import java.util.Formatter;
-import java.util.FormatterClosedException;
-import java.util.NoSuchElementException;
-import java.util.Scanner;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 /*
- * This class partitions a given file into equal parts. The method CreatePartition creates a number of files
- * specified by the user of approximately equally sized files
+ * This class partitions a given file into equal parts. The method CreatePartition creates a 
+ * number of files specified by the user of approximately equally sized files
  */
 public class FilePartitioner 
 {
-	private String fileName; 	//name of original file
+	private File oldFile; 	//original file
+	private String oldFileName; //name of original file
 	private double oldSize;		//size of original file
-	private static Scanner input;	//reads input from original file
-	private Formatter output;		//writes to the new files
+	//private static Scanner input;	//reads input from original file
+	private static FileInputStream input;
+	//private Formatter output;		//writes to the new files
+	private FileOutputStream output;
+	private static int MAX_INTEGER = 429496729;
 	
 	/*
 	 * Constructor for FilePartitioner 
@@ -23,28 +24,44 @@ public class FilePartitioner
 	 */
 	public FilePartitioner(String fileName)
 	{
-		this.fileName = fileName;
-		oldSize = new File(fileName).length();		
+		oldFile = new File(fileName);
+		oldFileName = fileName;
+		oldSize = oldFile.length();		
 	}
 	
 	/*
 	 * creates a user-specified-number of approximately equally sized files from the original file
-	 *@param numParts number of partitions the user wishes to divide the orignal file into
+	 *@param numParts number of partitions the user wishes to divide the original file into
 	 */
 
 	public void createPartition (int numParts)
 	{
-		int newSize = (int) oldSize / numParts; //size new files should be
 		openOldFile();
+						
 		for (int i = 1; i < numParts + 1; i++)
 		{
+			double newSize = oldSize / numParts;
 			openNewFile(i);
-			addNewData(newSize);
+			
+			while (newSize > 0)
+			{
+				if (newSize > MAX_INTEGER) //if the newSize can't be properly parsed to an int
+				{
+					addNewData(MAX_INTEGER); //add max number of data to file, and then repeat loop
+				}
+				else
+				{
+					addNewData((int) newSize);
+				}
+				newSize = newSize - MAX_INTEGER;
+			}
+			
 			closeNewFile();
 		}
-		closeOldFile();
 		
+		closeOldFile();		
 	}
+	
 /*
  * Opens the scanner to read the original file
  */
@@ -52,25 +69,26 @@ public class FilePartitioner
 	{
 		try
 		{
-			input = new Scanner(Paths.get(fileName));
+			input = new FileInputStream(oldFile);
 		}
 		catch (FileNotFoundException exception)
 		{
 			System.err.println("Error opening file. Terminating.");
 			System.exit(1);
 		}
+	}
+	
+	public void closeOldFile()
+	{
+		try
+		{
+			input.close();
+		}
 		catch (IOException exception)
 		{
 			System.err.println("Error opening file. Terminating.");
 			System.exit(1);
 		}
-		input.useDelimiter(""); //input.next() normally returns the whole string until whitespace
-								//this makes input.next() return just 1 character
-	}
-	
-	public void closeOldFile()
-	{
-		input.close();
 	}
 	
 	/*
@@ -80,10 +98,10 @@ public class FilePartitioner
 	public void openNewFile(int p)
 	{
 			//use a substring to remove the ".txt" from the original fileName
-			String newFileName = fileName.substring(0, fileName.length()-4) + "part" + p + ".txt";
+			String newFileName = oldFileName.substring(0, oldFileName.length()-4) + "part" + p + ".txt";
 			try
 			{
-				output = new Formatter(newFileName);
+				output = new FileOutputStream(newFileName);
 			}
 			
 			catch (FileNotFoundException fileNotFoundException)
@@ -93,33 +111,39 @@ public class FilePartitioner
 			} 
 	}
 	
+	public void closeNewFile()
+	{
+		try
+		{
+			if (output != null)
+				output.close();
+		}
+		catch (IOException exception)
+		{
+			System.err.println("Error adding data to file. Terminating.");
+			System.exit(1);
+		}
+	}
+	
 	/*
 	 * Writes data to the file.
 	 * @param int newFileSize is the desired size of the new files (ie: old size/number of parts)
 	 */
 	public void addNewData(int newFileSize)
 	{
-			for (int i = 0; i < newFileSize; i++)
-			{
-				try
-				{
-					output.format("%s",input.next());
-				}
-				catch (FormatterClosedException formatterClosedException)
-				{
-					System.err.println("Error writing to file. Terminating.");
-					break;
-				}
-				catch (NoSuchElementException noSuchElementException)
-				{
-					System.err.println("Invalid input. Please try again.");
-				}
-			}			
-	}
-
-	public void closeNewFile()
-	{
-		if (output != null)
-			output.close();
+		try
+		{
+			byte[] dataArray = new byte[newFileSize];
+			input.read(dataArray, 0, newFileSize); //fills the dataArray with buffered characters
+													//starts at the 0 position and adds newFileSize number of bytes
+			output.write(dataArray); //writes the contents of dataArray to output (FileOutputStream)
+		}
+			
+		catch (IOException exception)
+		{
+			System.err.println("Error adding data to file. Terminating.");
+			System.exit(1);
+		}
+			
 	}
 }
